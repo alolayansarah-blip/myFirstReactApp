0b50065c06188fcb9cfc4262e1610b5bb6bf0fbe
t000085-stack <t000085@coded.edu.kw>
2025-11-16 22:54:38 +0300

    Fix Heroku deployment - remove static export and configure PORT

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
+  // Removed static export for Heroku server deployment
   images: {
     unoptimized: true,
   },
